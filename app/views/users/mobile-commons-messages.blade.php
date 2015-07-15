6339dad77794330df2525a6160739b8c567f7aa3
Paul Han <CRABAPPLE@CRABAPPLEs-MacBook-Pro.local>
2015-07-15 17:23:18 -0400

    css for received,sent,failed messages

--- app/views/users/mobile-commons-messages.blade.php
+++ app/views/users/mobile-commons-messages.blade.php
@@ -6,17 +6,51 @@
 
 <div class ="container -padded">
   <div class="wrapper">
-	  <div class="container__block -narrow">
+	  <div class="container__block">
 	  	@forelse($mc_messages as $message)
-				<dl class="profile-settings">
-		  		<dt>Message Type:	</dt><dd>{{{ $message['@attributes']['message_type'] }}}</dd>
-		  		<dt>Status: </dt><dd>{{{ $message['@attributes']['status'] }}}</dd>
-		  		<dt>Campaign: </dt><dd>{{{ $message['campaign']['name'] }}}</dd>
-		  		<dt>Campaign ID: </dt><dd>{{{ $message['campaign']['@attributes']['id'] }}}</dd>
-		  		<dt>Active?: </dt><dd>{{{ $message['campaign']['@attributes']['active'] }}}</dd>
-		  		<dt>Sent On: </dt><dd>{{{ $message['when'] }}}</dd>
-		  		<dt>Content: </dt><dd>{{{ $message['body'] }}}</dd>
-				</dl>
+	  		@if ($message['@attributes']['status'] == "sent")
+					<div class="message-sent">
+						<div class="container__block">
+							<dl class="profile-settings">
+					  		<dt>Status: </dt><dd>{{{ $message['@attributes']['status'] }}}</dd>
+					  		<dt>Message Type:	</dt><dd>{{{ $message['@attributes']['message_type'] }}}</dd>
+					  		<dt>Campaign: </dt><dd>{{{ $message['campaign']['name'] }}}</dd>
+					  		<dt>Campaign ID: </dt><dd>{{{ $message['campaign']['@attributes']['id'] }}}</dd>
+					  		<dt>Active?: </dt><dd>{{{ $message['campaign']['@attributes']['active'] }}}</dd>
+					  		<dt>Sent On: </dt><dd>{{{ $message['when'] }}}</dd>
+					  		<dt>Content: </dt><dd>{{{ $message['body'] }}}</dd>
+							</dl>
+			  		</div>
+		  		</div>
+	  		@elseif ($message['@attributes']['status'] == "failed_permanently")
+  				<div class="message-failed">
+						<div class="container__block">
+							<dl class="profile-settings">
+					  		<dt>Status: </dt><dd>{{{ $message['@attributes']['status'] }}}</dd>
+					  		<dt>Message Type:	</dt><dd>{{{ $message['@attributes']['message_type'] }}}</dd>
+					  		<dt>Campaign: </dt><dd>{{{ $message['campaign']['name'] }}}</dd>
+					  		<dt>Campaign ID: </dt><dd>{{{ $message['campaign']['@attributes']['id'] }}}</dd>
+					  		<dt>Active?: </dt><dd>{{{ $message['campaign']['@attributes']['active'] }}}</dd>
+					  		<dt>Sent On: </dt><dd>{{{ $message['when'] }}}</dd>
+					  		<dt>Content: </dt><dd>{{{ $message['body'] }}}</dd>
+							</dl>
+			  		</div>
+		  		</div>
+	  		@else
+  				<div class="message-received">
+						<div class="container__block">
+							<dl class="profile-settings">
+					  		<dt>Status: </dt><dd>{{{ $message['@attributes']['status'] }}}</dd>
+					  		<dt>Message Type:	</dt><dd>{{{ $message['@attributes']['message_type'] }}}</dd>
+					  		<dt>Campaign: </dt><dd>{{{ $message['campaign']['name'] }}}</dd>
+					  		<dt>Campaign ID: </dt><dd>{{{ $message['campaign']['@attributes']['id'] }}}</dd>
+					  		<dt>Active?: </dt><dd>{{{ $message['campaign']['@attributes']['active'] }}}</dd>
+					  		<dt>Sent On: </dt><dd>{{{ $message['when'] }}}</dd>
+					  		<dt>Content: </dt><dd>{{{ $message['body'] }}}</dd>
+							</dl>
+			  		</div>
+		  		</div>
+	  		@endif
 	  	@empty
 	  		<h3>This user has no messages</h3>
 	  	@endforelse				
